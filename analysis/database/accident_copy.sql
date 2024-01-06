102dba35ca5a56452606ace035730ff034d7aad4
Sebastian Lenzlinger <74497638+sebaschi@users.noreply.github.com>
2024-01-06 16:39:58 +0100

    Add accident_copy.sql

---
+++ analysis/database/accident_copy.sql
@@ -0,0 +1,16 @@
+drop table if exists accident_copy;
+
+create table accident_copy as
+    select * from accidents;
+alter table accident_copy add severity varchar;
+update accident_copy set severity = 'Accident with property damage'
+where accidentseveritycategory='as4';
+
+update accident_copy set severity = 'Accident with light injuries'
+where accidentseveritycategory='as3';
+
+update accident_copy set severity = 'Accident with severe injuries'
+where accidentseveritycategory='as2';
+
+update accident_copy set severity = 'Accidents with fatalities'
+where accidentseveritycategory='as1';
